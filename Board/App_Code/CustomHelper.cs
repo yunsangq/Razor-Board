5a73fb512ef18f56eaaa194df41c687552b66c72
yunsangq <yunsangq@gmail.com>
2014-11-06 20:23:34 +0900

    Maker SIG ASP.NET Razor

---
+++ Board/App_Code/CustomHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+/// <summary>
+/// Summary description for ClassName
+/// </summary>
+public class CustomHelper
+{
+	public CustomHelper()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+    public static String DateTimeParser(string dateTimeString)
+    {
+        DateTime parseDatetime = DateTime.Now;
+        DateTime.TryParse(dateTimeString , out parseDatetime);
+        if(parseDatetime > DateTime.Parse(DateTime.Now.ToShortDateString()))
+        {
+            return parseDatetime.ToShortTimeString();
+        }
+        return parseDatetime.ToShortDateString();
+    }
+}
